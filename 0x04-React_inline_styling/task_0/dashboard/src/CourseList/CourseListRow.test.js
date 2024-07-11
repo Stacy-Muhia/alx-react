8fd53cf8888a7506fcc2edae3765ac902dd2dd4d
Stacy-Muhia <128844293+Stacy-Muhia@users.noreply.github.com>
2024-07-12 00:00:45 +0300

    0x04-React_inline_styling

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,18 +1,29 @@
-import React from "react";
-import Enzyme, { shallow } from 'enzyme'
-import CourseListRow from "./CourseListRow";
+import React from 'react';
+import { shallow } from 'enzyme';
+import CourseListRow from './CourseListRow';
 
-describe("Test CourseListRow component", () => {
-    test("renders one cell with colspan = 2 when textSecondCell does not exist and header is true", () => {
-        const wrapper = shallow(<CourseListRow  isHeader={true} textFirstCell="First cell"/>)
-        expect(wrapper.find('th').length).toBe(1)
-        expect(wrapper.find('th').text()).toBe("First cell")
+describe('CourseListRow component tests', () => {
+  it('renders one cell with colspan = 2 when isHeader is true and textSecondCell is null', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="First cell" />);
+    expect(wrapper.find('th')).toHaveLength(1);
+    expect(wrapper.find('th').prop('colSpan')).toEqual('2');
+    expect(wrapper.find('th').text()).toEqual('First cell');
+    expect(wrapper.find('tr').prop('style')).toEqual({ backgroundColor: '#deb5b545' });
+  });
 
-    })
-    test("renders two cells when textSecondCell is present", () => {
-        const wrapper = shallow(<CourseListRow  isHeader={true} textFirstCell="First cell" textSecondCell="Second cell" />)
-        expect(wrapper.find('th').length).toBe(2)
-        expect(wrapper.find('th').at(0).text()).toBe("First cell")
-        expect(wrapper.find('th').at(1).text()).toBe("Second cell")
-    })
-})
+  it('renders two cells when isHeader is true and textSecondCell is present', () => {
+    const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="First cell" textSecondCell="Second cell" />);
+    expect(wrapper.find('th')).toHaveLength(2);
+    expect(wrapper.find('th').at(0).text()).toEqual('First cell');
+    expect(wrapper.find('th').at(1).text()).toEqual('Second cell');
+    expect(wrapper.find('tr').prop('style')).toEqual({ backgroundColor: '#deb5b545' });
+  });
+
+  it('renders two td elements within a tr when isHeader is false', () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="First cell" textSecondCell="Second cell" />);
+    expect(wrapper.find('td')).toHaveLength(2);
+    expect(wrapper.find('td').at(0).text()).toEqual('First cell');
+    expect(wrapper.find('td').at(1).text()).toEqual('Second cell');
+    expect(wrapper.find('tr').prop('style')).toEqual({ backgroundColor: '#f5f5f5ab' });
+  });
+});
